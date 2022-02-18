5f5c064bc2a9c9a1cd9abc5da594bb33fbd78a81
unknown <seevaaluu@gmail.com>
2022-02-17 19:22:26 -0600

    advance

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+
+        for ($i=0; $i < 1000; $i++) { 
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $faker->password
+            ]);
+        }
+    }
+}
