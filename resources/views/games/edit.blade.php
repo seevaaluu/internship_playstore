8394d00ae9ff14cefe72b7c53b8fa2ca00caf660
unknown <seevaaluu@gmail.com>
2022-03-02 13:59:35 -0600

    addings

--- resources/views/games/edit.blade.php
+++ resources/views/games/edit.blade.php
@@ -1,90 +1,45 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('layouts.app')
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <title>Taller</title>
-  </head>
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Playstore</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ route('games.index') }}">Games</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm">
-                <h1>Editando {{ $game->name }}</h1>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm">
-               <form method="post" action="{{ route('games.update', $game->id) }}" enctype="multipart/form-data">
-                    @csrf
-                    @method('put')
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Nombre</label>
-                        <input value="{{ $game->name }}" type="text" name="name" class="form-control"  placeholder="Escribe el nombre del videojuego">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Descripcion</label>
-                        <input value="{{ $game->description }}" type="text" name="description" class="form-control"  placeholder="Escribe el valor">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Plataforma</label>
-                        <select required value="{{  $game->platform }}" class="form-control" name="platform">
-                            <option value="Playstation">Playstation</option>
-                            <option value="Xbox">Xbox</option>
-                            <option value="Nintendo switch">Nintendo switch</option>
-                            <option value="Otro">Otro</option>
-                        </select>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Precio</label>
-                        <input required value="{{ $game->price }}" type="number" name="price" class="form-control"  placeholder="Escribe el valor">
-                    </div>
-
-                    <div class="form-group">
-                        <input class="btn btn-primary" type="submit" value="Guardar">
-                    </div>
-                </form>
-            </div>
+@section('content')
+    <div class="row">
+        <div class="col-sm">
+            <h1>Editando {{ $game->name }}</h1>
         </div>
     </div>
+    <div class="row">
+        <div class="col-sm">
+            <form method="post" action="{{ route('games.update', $game->id) }}" enctype="multipart/form-data">
+                @csrf
+                @method('put')
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Nombre</label>
+                    <input value="{{ $game->name }}" type="text" name="name" class="form-control"  placeholder="Escribe el nombre del videojuego">
+                </div>
 
-    @if(count($errors) > 0)
-        <div class="alert alert-danger">
-            <p>Corrige los siguientes errores:</p>
-            <ul>
-                @foreach ($errors->all() as $message)
-                    <li>{{ $message }}</li>
-                @endforeach
-            </ul>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Descripcion</label>
+                    <input value="{{ $game->description }}" type="text" name="description" class="form-control"  placeholder="Escribe el valor">
+                </div>
+
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Plataforma</label>
+                    <select required value="{{  $game->platform }}" class="form-control" name="platform">
+                        <option value="Playstation">Playstation</option>
+                        <option value="Xbox">Xbox</option>
+                        <option value="Nintendo switch">Nintendo switch</option>
+                        <option value="Otro">Otro</option>
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Precio</label>
+                    <input required value="{{ $game->price }}" type="number" name="price" class="form-control"  placeholder="Escribe el valor">
+                </div>
+
+                <div class="form-group">
+                    <input class="btn btn-primary" type="submit" value="Guardar">
+                </div>
+            </form>
         </div>
-    @endif
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
-</html>
+    </div>
+@endsection
--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -1,36 +1,12 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('layouts.app')
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <title>Taller</title>
-  </head>
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Playstore</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ route('games.index') }}">Games</a>
-                    </li>
-                </ul>
-            </div>
+@section('content')
+    <div class="row hidden-md-up">
+        <div class="col-md-6">
+            <h1>Videojuegos</h1>
         </div>
-    </nav>
-    <div class="container">
-        <div class="row hidden-md-up">
-            <div class="col-md-6">
-                <h1>Videojuegos</h1>
-            </div>
+        <div class="col-md-6 py-2">
+            <a class="btn btn-primary btn-outline" href="{{ route('games.create') }}"> Añadir nuevo videojuego</a>
         </div>
         <div class="py-5">
             <div class="container">
@@ -38,7 +14,7 @@
                     @foreach($games as $key => $game)
                         <div class="col-md-4">
                             <div class="card" style="width: 18rem;">
-                                <img src="{{ $game->cover }}" class="card-img-top" alt="...">
+                                <img src="{{ $game->cover }}" class="card-img-top img-fluid img-thumbnail" alt="cover">
                                 <div class="card-body">
                                     <h5 class="card-title">{{ $game->name }}</h5>
                                     <p class="card-text">{{ $game->description }}</p>
@@ -50,7 +26,7 @@
                                 <div class="card-body">
                                     <div class="row">
                                         <div class="col-md-6">
-                                            <a class="btn btn-success" href="{{ route('games.edit', $game->id) }}">
+                                            <a class="btn btn-success btn-block" href="{{ route('games.edit', $game->id) }}">
                                                 Editar
                                             </a>
                                         </div>
@@ -73,17 +49,8 @@
                     <div class="col-md-6">
                         {{ $games->links() }}
                     </div>
-                    <div class="col-md-6">
-                        <a class="btn btn-primary" href="{{ route('games.create') }}"> Añadir nuevo videojuego</a>
-                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
-</html>
+@endsection
